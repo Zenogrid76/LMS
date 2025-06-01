db210027e8c1795cfd541a641919040b7271bc08
Mahmud Reza <124916971+Zenogrid76@users.noreply.github.com>
2025-06-01 06:12:59 +0600

    lms 0.3

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend project.
+URL configuration for api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.students.urls')),  # Include the students app URLs
 ]
